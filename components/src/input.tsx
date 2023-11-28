81a07e4ffe4883207b1c2bfd5fdcf882cc5c399a
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-11-28 22:18:20 +0100

    feat: add auto hash for password input

--- components/src/input.tsx
+++ components/src/input.tsx
@@ -1,10 +1,38 @@
-import { JSX } from '../../deps.ts'
+import { effect, JSX, signal } from '../../deps.ts'
 
 export function Input(
 	{ label, name, ...props }:
 		& { label: string; name: string }
 		& JSX.HTMLAttributes<HTMLInputElement>,
 ) {
+	if (props.type === 'password') {
+		const password = signal('')
+		const hash = signal('')
+		const salt = crypto.randomUUID()
+
+		effect(async () => {
+			const digest = await crypto.subtle.digest(
+				'SHA-256',
+				new TextEncoder().encode(salt + password.value),
+			)
+			hash.value = new TextDecoder().decode(digest)
+		})
+
+		return (
+			<label>
+				<span>{label}</span>
+				<input
+					{...props}
+					onInput={(event) =>
+						password.value =
+							(event.target as HTMLInputElement).value}
+				/>
+				<input type='text' hidden value={hash} name={name} />
+				<input type='text' hidden value={salt} name={`${name}_salt`} />
+			</label>
+		)
+	}
+
 	return (
 		<label>
 			<span>{label}</span>
