738e217de3dfabe5636f67ca8436dce10865982e
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-11-29 01:01:56 +0100

    chore: upgrade preact version

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { computed, effect, signal } from 'https://esm.sh/@preact/signals@1.2.1'
-export * as preact from 'https://esm.sh/preact@10.15.1'
-export type { JSX } from 'https://esm.sh/preact@10.15.1'
+export * as preact from 'https://esm.sh/preact@10.19.2'
+export type { JSX } from 'https://esm.sh/preact@10.19.2'
