a85f91ee56216c71f5156045c893489ed938aeb0
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-01-13 23:00:06 +0100

    fix: update dependencies exports

--- deps.ts
+++ deps.ts
@@ -1,3 +1,22 @@
-export { computed, effect, signal } from 'https://esm.sh/@preact/signals@1.2.1'
+export * as path from 'https://deno.land/std@0.181.0/path/mod.ts'
+export { bundle } from 'https://deno.land/x/emit@0.24.0/mod.ts'
+export * as gfm from 'https://deno.land/x/gfm@0.2.1/mod.ts'
+export {
+	computed,
+	effect,
+	signal,
+	useComputed,
+	useSignal,
+	useSignalEffect,
+} from 'https://esm.sh/@preact/signals@1.2.1?deps=preact@10.19.2'
+export type {
+	Signal,
+} from 'https://esm.sh/@preact/signals@1.2.1?deps=preact@10.19.2'
+export {
+	useEffect,
+	useMemo,
+	useRef,
+} from 'https://esm.sh/preact@10.15.1/hooks?deps=preact@10.19.2'
+export { default as ssr } from 'https://esm.sh/preact-render-to-string@5.2.6?deps=preact@10.19.2'
 export * as preact from 'https://esm.sh/preact@10.19.2'
 export type { JSX } from 'https://esm.sh/preact@10.19.2'
